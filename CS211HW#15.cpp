48186b027fc22bf7d1eaaddeb3f862c23e63f7c6
Eli Kleinworm <elik12996@gmail.com>
2017-12-26 17:07:45 -0500

    Towers of Hanoi
    Towers of Hanoi

---
+++ CS211HW#15.cpp
@@ -0,0 +1,59 @@
+/* CS 211 HW#15
+   Towers
+*/    
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> t[3]; // three towers A,B,C represented as an array of 3 
+    int n, candidate,to, from, move=0; // move counts the number of moves
+    cout<<"Please enter a number of rings to move: ";
+    cin>>n;
+    cout<<endl;
+
+    for(int i=n+1;i>=1;i--)
+        t[0].push_back(i);
+
+    t[1].push_back(n+1);
+    t[2].push_back(n+1);
+
+    if((n%2)!=0){
+        from=0;
+        to=1;
+        candidate=1;
+    }
+    else{
+        from=0;
+        to=2;
+        candidate=1;
+    }
+
+    while(t[1].size()<n+1){ 
+        cout<<"move number "<<++move<<": Transfer ring "<<candidate<<
+        " from tower "<< char(from+65)<<" to tower "<<char(to+65)<<endl;
+
+        t[to].push_back(t[from].back());
+        t[from].pop_back();
+
+        if(t[(to+1)%3].back()<t[(to+2)%3].back()) from=(to+1)%3;
+        else from=(to+2)%3;
+
+        //odd number of rings
+        if((n%2)!=0){
+            //"to" = closest tower on which the ring can be placed
+            if(t[from].back()<t[(from+1)%3].back()) to=(from+1)%3;
+            else to=(from+2)%3;
+        }
+
+        //even number of rings
+        else{
+            if(t[from].back()<t[(from+2)%3].back()) to=(from+2)%3;
+            else to=(from+1)%3;
+        }
+
+        candidate=t[from].back();
+    }
+    return 0;
+}
